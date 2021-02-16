0c1f69c6a3a722abb7692a784ba55a6b082ec017
Bazztoner <bazztoner.phoenix@gmail.com>
2021-02-16 13:36:14 -0300

    Added virtual to all functions on baseProjectile

--- Assets/Script/Entities/Projectiles/EnemyProjectileDefault.cs
+++ Assets/Script/Entities/Projectiles/EnemyProjectileDefault.cs
@@ -5,34 +5,5 @@ using System.Linq;
 
 public class EnemyProjectileDefault : BaseProjectile
 {
-    Rigidbody2D _rb;
 
-    void Awake()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
-
-    public void SpawnProjectile(Vector3 position, Vector3 direction)
-    {
-        transform.position = position;
-        transform.up = direction;
-        //owner = entityOwner
-    }
-
-    void FixedUpdate()
-    {
-        _rb.MovePosition(transform.up * speed * Time.fixedDeltaTime);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log($"{gameObject.name} collided into {collision.gameObject.name}");
-        Destroy(gameObject);
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log($"{gameObject.name} collided into {collision.gameObject.name}");
-        Destroy(gameObject);
-    }
 }
--- Assets/Script/Player/BaseProjectile.cs
+++ Assets/Script/Player/BaseProjectile.cs
@@ -10,30 +10,39 @@ public class BaseProjectile : MonoBehaviour
 
     public float speed;
 
-    void Awake()
+    protected Entity _owner;
+
+    protected virtual void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    public void SpawnProjectile(Vector3 position, Vector3 direction)
+    public virtual void SpawnProjectile(Vector3 position, Vector3 direction)
     {
         transform.position = position;
         transform.up = direction;
         //owner = entityOwner
     }
 
-    void FixedUpdate()
+    public virtual void SpawnProjectile(Vector3 position, Vector3 direction, Entity owner)
+    {
+        transform.position = position;
+        transform.up = direction;
+        _owner = owner;
+    }
+
+    protected virtual void FixedUpdate()
     {
         _rb.MovePosition(transform.up * speed * Time.fixedDeltaTime);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log($"{gameObject.name} collided into {collision.gameObject.name}");
         Destroy(gameObject);
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log($"{gameObject.name} collided into {collision.gameObject.name}");
         Destroy(gameObject);
