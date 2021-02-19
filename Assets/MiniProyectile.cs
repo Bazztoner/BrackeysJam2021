ad259f8bb825ed39b2a3d841932bda95892fe04f
DLeoGDesign <66979073+DLeoGDesign@users.noreply.github.com>
2021-02-19 01:51:48 -0300

    Progression
    ADDED - GameManager.cs for saving player progression during play.

--- Assets/MiniProyectile.cs
+++ Assets/MiniProyectile.cs
@@ -8,6 +8,8 @@ public class MiniProyectile : MonoBehaviour
 
     Transform destination;
 
+    Vector3 _target =  Vector3.zero;
+
     Vector3 dir;
 
     public float speed;
@@ -24,12 +26,14 @@ public class MiniProyectile : MonoBehaviour
 
     public void SetDestination(Vector3 _destination)
     {
-        destination.position = _destination;
+        _target = _destination;
     }
 
     private void LateUpdate()
     {
-        if (destination != null) dir = destination.position - transform.position;
+        if (destination != null) _target = destination.position;
+        
+        dir = _target - transform.position;
 
         transform.up = dir;
 
