54604df63899fddd480a47d164c8b66e0966bc22
DLeoGDesign <66979073+DLeoGDesign@users.noreply.github.com>
2021-02-20 08:46:47 -0300

    Transition to Planets
    ADDED - Colliders that transition the player into the different planets.
    ADDED - Logic to unlock sucessive planets after completing a previous one.
    ADDED - Logic that spawns the player after the last planet they succesfully completed, both after completing it or Cotinuing from MainMenu.

---
+++ Assets/Script/SceneManagers/HUBManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUBManager : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform player;
+
+    [SerializeField]
+    protected Transform[] pointsStarting = new Transform[6];
+
+    [SerializeField]
+    protected PlanetOrbit[] planets;
+
+    [SerializeField]
+    protected Image rend;
+
+    [SerializeField]
+    protected GameObject hud;
+
+    private void Start()
+    {
+        for (int i = 0; i < pointsStarting.Length; i++)
+        {
+            if (GameManager.manager.GetPlanetUnlocked(i))
+            {
+                planets[i].gameObject.SetActive(false);
+                player.position = pointsStarting[i].position;
+            }
+            else break;
+        }
+
+        StartCoroutine(LightUp());
+    }
+
+    IEnumerator LightUp()
+    {
+        Color end = new Color(0f, 0f, 0f, 0f);
+
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            rend.color = Color.Lerp(Color.black, end, t);
+
+            yield return new WaitForEndOfFrame();
+
+            t += Time.deltaTime;
+        }
+
+        hud.SetActive(true);
+    }
+
+    public void Transitioin(int i)
+    {
+        StartCoroutine(LightDown(i));
+    }
+
+    IEnumerator LightDown(int i)
+    {
+        Color init = new Color(0f, 0f, 0f, 0f);
+
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            rend.color = Color.Lerp(init, Color.black, t);
+
+            yield return new WaitForEndOfFrame();
+
+            t += Time.deltaTime;
+        }
+
+        EnterPlanet(i);
+    }
+
+    public void EnterPlanet(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                GameManager.manager.LoadScene(GameScenes.P1L1);
+                break;
+            case 1:
+                GameManager.manager.LoadScene(GameScenes.P2L1);
+                break;
+            case 2:
+                GameManager.manager.LoadScene(GameScenes.P3L1);
+                break;
+            case 3:
+                GameManager.manager.LoadScene(GameScenes.P4L1);
+                break;
+            case 4:
+                GameManager.manager.LoadScene(GameScenes.P5L1);
+                break;
+            case 5:
+                GameManager.manager.LoadScene(GameScenes.FinalBoss);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Script/SceneManagers/PlanetOrbit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlanetOrbit : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform planet, player;
+
+    public UnityEvent onEnterOrbit;
+
+    IEnumerator EnteringOrbit()
+    {
+        Vector3 initPos = player.position;
+
+        bool called = false;
+
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            player.position = Vector3.Lerp(initPos, planet.position, t);
+
+            yield return new WaitForEndOfFrame();
+
+            t += Time.deltaTime;
+
+            if ((t > .25f) && !called)
+            {
+                onEnterOrbit.Invoke();
+                called = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.LayerMatchesWith("Player")) 
+            StartCoroutine(EnteringOrbit());
+    }
+}
