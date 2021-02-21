ca98e388f7d03537cbb5df6618b938b2d28e67db
Bazztoner <bazztoner.phoenix@gmail.com>
2021-02-21 00:41:08 -0300

    Leo's changes v2

---
+++ Assets/Script/UI/UIAvailableSeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIAvailableSeed : MonoBehaviour
+{
+    [SerializeField]
+    protected TextMeshProUGUI ammo;
+
+    [SerializeField]
+    protected SeedTypes type;
+
+    void Start()
+    {
+        if (ammo == null) ammo = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void UpdateAmmo()
+    {
+        ammo.text = GameManager.Instance.GetRemainingSeeds()[(int)type].ToString();
+    }
+}
