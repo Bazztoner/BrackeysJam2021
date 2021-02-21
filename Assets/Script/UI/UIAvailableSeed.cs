f98109db4b4656fe228edb92496ecc85d3500ef1
Bazztoner <bazztoner.phoenix@gmail.com>
2021-02-21 01:15:09 -0300

    HUD art update

--- Assets/Script/UI/UIAvailableSeed.cs
+++ Assets/Script/UI/UIAvailableSeed.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class UIAvailableSeed : MonoBehaviour
 {
@@ -11,9 +12,17 @@ public class UIAvailableSeed : MonoBehaviour
     [SerializeField]
     protected SeedTypes type;
 
+    GameObject _lockGraphic;
+
     void Start()
     {
         if (ammo == null) ammo = GetComponentInChildren<TextMeshProUGUI>();
+        _lockGraphic = GetComponentsInChildren<Transform>().Where(x => x.gameObject.name == "Lock").Select(x =>x.gameObject).FirstOrDefault();
+    }
+
+    void Update()
+    {
+        _lockGraphic.SetActive(GameManager.Instance.GetUnlockedSeeds()[(int)type]);
     }
 
     public void UpdateAmmo()
